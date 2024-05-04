59a369ba4dc5dc3d0dbd0b100272f0ffda02b71f
madeline <madgrismad@gmail.com>
2024-05-04 05:09:19 -0700

    feat: Agregar funcionalidad de cálculo de pago según tipo de semana y días asistidos

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,85 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Registro de Semanas</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content class="ion-padding">
+      <ion-item>
+        <ion-label>Semana:</ion-label>
+        <ion-select v-model="tipoSemana">
+          <ion-select-option value="corta">Semana Corta</ion-select-option>
+          <ion-select-option value="larga">Semana Larga</ion-select-option>
+        </ion-select>
+      </ion-item>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-item>
+        <ion-label>Días Asistidos:</ion-label>
+        <ion-input v-model.number="diasAsistidos" type="number"></ion-input>
+      </ion-item>
+
+      <ion-button @click="calcularPago()">Calcular Pago</ion-button>
+
+      <ion-item v-if="totalPagar !== null">
+        <ion-label>Total a Pagar:</ion-label>
+        <ion-text>{{ totalPagar }} pesos</ion-text>
+      </ion-item>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+export default {
+  data() {
+    return {
+      tipoSemana: 'corta', // Valor por defecto: semana corta
+      diasAsistidos: 0,
+      totalPagar: null
+    };
+  },
+  methods: {
+    calcularPago() {
+      if (this.tipoSemana === 'larga') {
+        // Calcula el total a pagar para una semana larga (4 días)
+        switch (this.diasAsistidos) {
+          case 4:
+            this.totalPagar = 500; // Asistió todos los días
+            break;
+          case 3:
+            this.totalPagar = 400;
+            break;
+          case 2:
+            this.totalPagar = 300;
+            break;
+          case 1:
+            this.totalPagar = 200;
+            break;
+          default:
+            this.totalPagar = 100; // No asistió a ningún día
+        }
+      } else if (this.tipoSemana === 'corta') {
+        // Calcula el total a pagar para una semana corta (3 días)
+        switch (this.diasAsistidos) {
+          case 3:
+            this.totalPagar = 500; // Asistió todos los días
+            break;
+          case 2:
+            this.totalPagar = 400;
+            break;
+          case 1:
+            this.totalPagar = 300;
+            break;
+          default:
+            this.totalPagar = 200; // No asistió a ningún día
+        }
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
+/* Estilos CSS personalizados aquí */
 </style>
